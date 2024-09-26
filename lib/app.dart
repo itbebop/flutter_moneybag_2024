56c7f26cd997356ecb5076be5d6255417198a29d
itbebop <it.bebop@gmail.com>
2024-09-26 15:59:42 +0900

    feat: add ScreenUtilInit package and apply

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_moneybag_2024/common/common.dart';
 import 'package:flutter_moneybag_2024/router/router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -13,14 +14,19 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     // ProviderScope를 포함하여 GoRouter를 사용한 앱 구조로 변경
-    return ProviderScope(
-      child: Consumer(
-        builder: (context, ref, _) {
-          final router = ref.watch(routerProvider);
-          return MaterialApp.router(
-            routerConfig: router, // GoRouter 설정
-          );
-        },
+    return ScreenUtilInit(
+      designSize: const Size(390, 844),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: ProviderScope(
+        child: Consumer(
+          builder: (context, ref, _) {
+            final router = ref.watch(routerProvider);
+            return MaterialApp.router(
+              routerConfig: router, // GoRouter 설정
+            );
+          },
+        ),
       ),
     );
   }
--- lib/common/common.dart
+++ lib/common/common.dart
@@ -1,1 +1,3 @@
 export 'package:flutter_moneybag_2024/common/dart/extension/num_extension.dart';
+export 'package:flutter_moneybag_2024/common/dart/extension/num_duration_extension.dart';
+export 'package:flutter_screenutil/flutter_screenutil.dart';
