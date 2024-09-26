e0fcefeb24e6f4381c92e03a5ade7b9845bcecd4
itbebop <it.bebop@gmail.com>
2024-09-26 12:57:35 +0900

    style: theme, color setting

---
+++ lib/common/theme/color/abstract_theme_color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class AbstractThemeColors {
+  const AbstractThemeColors();
+
+  Color get mainColor => const Color(0x008cb9bd);
+}
---
+++ lib/common/theme/color/light_theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_moneybag_2024/common/theme/color/abstract_theme_color.dart';
+
+class LightAppColors extends AbstractThemeColors {
+  const LightAppColors();
+}
