1668a7e5d489b3c468f931178fe605c2e5fafbc5
itbebop <it.bebop@gmail.com>
2024-09-27 18:16:17 +0900

    style: traction model 이름 변경

--- lib/domain/model/transaction_detail.dart
+++ lib/domain/model/transaction_detail.dart
@@ -2,16 +2,16 @@ import 'package:flutter_moneybag_2024/domain/enums/asset_types.dart';
 import 'package:flutter_moneybag_2024/domain/model/user.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-part 'transaction.freezed.dart';
+part 'transaction_detail.freezed.dart';
 
 @freezed
-class Transaction with _$Transaction {
-  const factory Transaction({
+class TransactionDetail with _$TransactionDetail {
+  const factory TransactionDetail({
     required String title,
     required String imgUrl,
     required DateTime createdAt,
     required DateTime updatedAt,
     required List<User> user,
     required AssetType assetType,
-  }) = _Transaction;
+  }) = _TransactionDetail;
 }
--- lib/domain/model/transaction_detail.freezed.dart
+++ lib/domain/model/transaction_detail.freezed.dart
@@ -3,7 +3,7 @@
 // ignore_for_file: type=lint
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
-part of 'transaction.dart';
+part of 'transaction_detail.dart';
 
 // **************************************************************************
 // FreezedGenerator
@@ -15,7 +15,7 @@ final _privateConstructorUsedError = UnsupportedError(
     'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
 
 /// @nodoc
-mixin _$Transaction {
+mixin _$TransactionDetail {
   String get title => throw _privateConstructorUsedError;
   String get imgUrl => throw _privateConstructorUsedError;
   DateTime get createdAt => throw _privateConstructorUsedError;
@@ -24,15 +24,15 @@ mixin _$Transaction {
   AssetType get assetType => throw _privateConstructorUsedError;
 
   @JsonKey(ignore: true)
-  $TransactionCopyWith<Transaction> get copyWith =>
+  $TransactionDetailCopyWith<TransactionDetail> get copyWith =>
       throw _privateConstructorUsedError;
 }
 
 /// @nodoc
-abstract class $TransactionCopyWith<$Res> {
-  factory $TransactionCopyWith(
-          Transaction value, $Res Function(Transaction) then) =
-      _$TransactionCopyWithImpl<$Res, Transaction>;
+abstract class $TransactionDetailCopyWith<$Res> {
+  factory $TransactionDetailCopyWith(
+          TransactionDetail value, $Res Function(TransactionDetail) then) =
+      _$TransactionDetailCopyWithImpl<$Res, TransactionDetail>;
   @useResult
   $Res call(
       {String title,
@@ -44,9 +44,9 @@ abstract class $TransactionCopyWith<$Res> {
 }
 
 /// @nodoc
-class _$TransactionCopyWithImpl<$Res, $Val extends Transaction>
-    implements $TransactionCopyWith<$Res> {
-  _$TransactionCopyWithImpl(this._value, this._then);
+class _$TransactionDetailCopyWithImpl<$Res, $Val extends TransactionDetail>
+    implements $TransactionDetailCopyWith<$Res> {
+  _$TransactionDetailCopyWithImpl(this._value, this._then);
 
   // ignore: unused_field
   final $Val _value;
@@ -93,11 +93,11 @@ class _$TransactionCopyWithImpl<$Res, $Val extends Transaction>
 }
 
 /// @nodoc
-abstract class _$$TransactionImplCopyWith<$Res>
-    implements $TransactionCopyWith<$Res> {
-  factory _$$TransactionImplCopyWith(
-          _$TransactionImpl value, $Res Function(_$TransactionImpl) then) =
-      __$$TransactionImplCopyWithImpl<$Res>;
+abstract class _$$TransactionDetailImplCopyWith<$Res>
+    implements $TransactionDetailCopyWith<$Res> {
+  factory _$$TransactionDetailImplCopyWith(_$TransactionDetailImpl value,
+          $Res Function(_$TransactionDetailImpl) then) =
+      __$$TransactionDetailImplCopyWithImpl<$Res>;
   @override
   @useResult
   $Res call(
@@ -110,11 +110,11 @@ abstract class _$$TransactionImplCopyWith<$Res>
 }
 
 /// @nodoc
-class __$$TransactionImplCopyWithImpl<$Res>
-    extends _$TransactionCopyWithImpl<$Res, _$TransactionImpl>
-    implements _$$TransactionImplCopyWith<$Res> {
-  __$$TransactionImplCopyWithImpl(
-      _$TransactionImpl _value, $Res Function(_$TransactionImpl) _then)
+class __$$TransactionDetailImplCopyWithImpl<$Res>
+    extends _$TransactionDetailCopyWithImpl<$Res, _$TransactionDetailImpl>
+    implements _$$TransactionDetailImplCopyWith<$Res> {
+  __$$TransactionDetailImplCopyWithImpl(_$TransactionDetailImpl _value,
+      $Res Function(_$TransactionDetailImpl) _then)
       : super(_value, _then);
 
   @pragma('vm:prefer-inline')
@@ -127,7 +127,7 @@ class __$$TransactionImplCopyWithImpl<$Res>
     Object? user = null,
     Object? assetType = null,
   }) {
-    return _then(_$TransactionImpl(
+    return _then(_$TransactionDetailImpl(
       title: null == title
           ? _value.title
           : title // ignore: cast_nullable_to_non_nullable
@@ -158,8 +158,8 @@ class __$$TransactionImplCopyWithImpl<$Res>
 
 /// @nodoc
 
-class _$TransactionImpl implements _Transaction {
-  const _$TransactionImpl(
+class _$TransactionDetailImpl implements _TransactionDetail {
+  const _$TransactionDetailImpl(
       {required this.title,
       required this.imgUrl,
       required this.createdAt,
@@ -189,14 +189,14 @@ class _$TransactionImpl implements _Transaction {
 
   @override
   String toString() {
-    return 'Transaction(title: $title, imgUrl: $imgUrl, createdAt: $createdAt, updatedAt: $updatedAt, user: $user, assetType: $assetType)';
+    return 'TransactionDetail(title: $title, imgUrl: $imgUrl, createdAt: $createdAt, updatedAt: $updatedAt, user: $user, assetType: $assetType)';
   }
 
   @override
   bool operator ==(Object other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
-            other is _$TransactionImpl &&
+            other is _$TransactionDetailImpl &&
             (identical(other.title, title) || other.title == title) &&
             (identical(other.imgUrl, imgUrl) || other.imgUrl == imgUrl) &&
             (identical(other.createdAt, createdAt) ||
@@ -215,18 +215,19 @@ class _$TransactionImpl implements _Transaction {
   @JsonKey(ignore: true)
   @override
   @pragma('vm:prefer-inline')
-  _$$TransactionImplCopyWith<_$TransactionImpl> get copyWith =>
-      __$$TransactionImplCopyWithImpl<_$TransactionImpl>(this, _$identity);
+  _$$TransactionDetailImplCopyWith<_$TransactionDetailImpl> get copyWith =>
+      __$$TransactionDetailImplCopyWithImpl<_$TransactionDetailImpl>(
+          this, _$identity);
 }
 
-abstract class _Transaction implements Transaction {
-  const factory _Transaction(
+abstract class _TransactionDetail implements TransactionDetail {
+  const factory _TransactionDetail(
       {required final String title,
       required final String imgUrl,
       required final DateTime createdAt,
       required final DateTime updatedAt,
       required final List<User> user,
-      required final AssetType assetType}) = _$TransactionImpl;
+      required final AssetType assetType}) = _$TransactionDetailImpl;
 
   @override
   String get title;
@@ -242,6 +243,6 @@ abstract class _Transaction implements Transaction {
   AssetType get assetType;
   @override
   @JsonKey(ignore: true)
-  _$$TransactionImplCopyWith<_$TransactionImpl> get copyWith =>
+  _$$TransactionDetailImplCopyWith<_$TransactionDetailImpl> get copyWith =>
       throw _privateConstructorUsedError;
 }
