a1195e21fe13bba2dc78d5b4b28348c10284f13c
itbebop <it.bebop@gmail.com>
2024-09-27 18:15:48 +0900

    feat: date extension 추가

--- lib/common/common.dart
+++ lib/common/common.dart
@@ -1,3 +1,4 @@
 export 'package:flutter_moneybag_2024/common/dart/extension/num_extension.dart';
 export 'package:flutter_moneybag_2024/common/dart/extension/num_duration_extension.dart';
 export 'package:flutter_screenutil/flutter_screenutil.dart';
+export 'package:flutter_moneybag_2024/common/dart/extension/date_extension.dart';
---
+++ lib/common/dart/extension/date_extension.dart
@@ -0,0 +1,3 @@
+import 'package:intl/intl.dart';
+
+DateFormat dateFormat = DateFormat('aa hh:mm', 'ko');
