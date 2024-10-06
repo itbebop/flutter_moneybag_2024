dea879bb77e40544ddd289111ad0232f1185a2f3
itbebop <it.bebop@gmail.com>
2024-10-06 11:54:37 +0900

    syle: home screen ui 조정

--- lib/screen/tab/home/home_screen.dart
+++ lib/screen/tab/home/home_screen.dart
@@ -1,8 +1,11 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_moneybag_2024/common/common.dart';
-import 'package:flutter_moneybag_2024/common/common_component/sort_button.dart';
-import 'package:flutter_moneybag_2024/common/common_component/transaction/transaction_list.dart';
+import 'package:flutter_moneybag_2024/common/common_component/transaction/transaction_item.dart';
 import 'package:flutter_moneybag_2024/common/data/month_list.dart';
+import 'package:flutter_moneybag_2024/domain/model/dummies.dart';
+import 'package:flutter_moneybag_2024/domain/model/transaction_detail.dart';
 import 'package:flutter_moneybag_2024/screen/tab/home/component/asset_list.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
@@ -16,88 +19,212 @@ class HomeScreen extends ConsumerStatefulWidget {
 }
 
 class _HomeScreenState extends ConsumerState<HomeScreen> {
-  String currentMonth = DateFormat('MMM').format(DateTime.now()).toLowerCase();
+  final String _currentMonth = DateFormat('MMM').format(DateTime.now()).toLowerCase();
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
   MonthList selectedMonth = MonthList.jan; // 초기값으로 설정
+  final CalendarFormat _calendarFormat = CalendarFormat.month;
+  final ValueNotifier<List<TransactionDetail>> _selectedEvents = ValueNotifier([]);
 
   @override
   void initState() {
     super.initState();
+    _selectedDay = _focusedDay;
+    _selectedEvents.value = _getEventsForDay(_selectedDay!);
     // 현재 월에 해당하는 MonthList 항목으로 selectedMonth 초기화
     selectedMonth = MonthList.values.firstWhere(
-      (e) => e.toString().split('.')[1] == currentMonth,
+      (e) => e.toString().split('.')[1] == _currentMonth,
     );
+  }
+
+  @override
+  void dispose() {
+    _selectedEvents.dispose();
+    super.dispose();
+  }
+
+  List<TransactionDetail> _getEventsForDay(DateTime day) {
+    return _kTransactionEvents[day] ?? [];
+  }
+
+  // 트랜잭션 데이터를 날짜별로 매핑
+  static final Map<DateTime, List<TransactionDetail>> _kTransactionEvents = LinkedHashMap<DateTime, List<TransactionDetail>>(
+    equals: isSameDay,
+    hashCode: (DateTime key) => key.day * 1000000 + key.month * 10000 + key.year,
+  )..addAll(_generateSampleTransactions());
+  static Map<DateTime, List<TransactionDetail>> _generateSampleTransactions() {
+    DateTime now = DateTime.now();
+    return {
+      now: [],
+      now.subtract(const Duration(days: 1)): [
+        TransactionDetail(
+          transactionId: 11,
+          title: '커피 2잔',
+          amount: -12000,
+          imgUrl: 'https://picsum.photos/203',
+          createdAt: now.subtract(const Duration(days: 1)),
+          updatedAt: now.subtract(const Duration(minutes: 10)),
+          user: [user1],
+          category: transactionCategory4,
+        ),
+        TransactionDetail(
+          transactionId: 12,
+          title: '점심(피자)',
+          amount: -30000,
+          imgUrl: 'https://picsum.photos/204',
+          createdAt: now.subtract(const Duration(days: 1)),
+          updatedAt: now.subtract(const Duration(minutes: 10)),
+          user: [user1],
+          category: transactionCategory3,
+        ),
+        TransactionDetail(
+          transactionId: 13,
+          title: '파킹통장 이자',
+          amount: 86000,
+          imgUrl: 'https://picsum.photos/206',
+          createdAt: now.subtract(const Duration(days: 1)),
+          updatedAt: now.subtract(const Duration(minutes: 10)),
+          user: [user1],
+          category: transactionCategory1,
+        ),
+      ],
+      now.subtract(const Duration(days: 2)): [
+        TransactionDetail(
+          transactionId: 14,
+          title: '9월 월급',
+          amount: 5600000,
+          imgUrl: 'https://picsum.photos/206',
+          createdAt: now.subtract(const Duration(days: 2)),
+          updatedAt: now.subtract(const Duration(minutes: 10)),
+          user: [user1, user2],
+          category: transactionCategory2,
+        ),
+        TransactionDetail(
+          transactionId: 15,
+          title: '미용',
+          amount: -20000,
+          imgUrl: 'https://picsum.photos/206',
+          createdAt: now.subtract(const Duration(days: 2)),
+          updatedAt: now.subtract(const Duration(minutes: 10)),
+          user: [user1],
+          category: transactionCategory2,
+        ),
+      ],
+    };
+  }
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      _selectedDay = selectedDay;
+      _focusedDay = focusedDay;
+    });
+
+    _selectedEvents.value = _getEventsForDay(selectedDay);
   }
 
   @override
   Widget build(BuildContext context) {
     DateTime now = DateTime.now();
     DateTime focusedDay = DateTime.utc(now.year, selectedMonth.month, now.day);
+    // final int selectedDay = focusedDay.day;
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Column(
+        // Column으로 변경
+        children: [
+          Expanded(
+            // Expanded를 Column 안으로 이동
+            child: SingleChildScrollView(
+              child: Column(
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        '${(DateTime.now().year).toString().substring(2)}년',
-                        style: const TextStyle(
-                          fontSize: 24,
-                          color: Colors.black,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 32),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '${(DateTime.now().year).toString().substring(2)}년',
+                              style: const TextStyle(
+                                fontSize: 16,
+                                color: Colors.black,
+                              ),
+                            ),
+                            MonthDropdownButton(
+                              selectedMonth: selectedMonth,
+                              onMonthChanged: (value) {
+                                setState(() {
+                                  selectedMonth = value; // 선택된 월 업데이트
+                                });
+                              },
+                              fontSize: 24,
+                            ),
+                          ],
                         ),
-                      ),
-                      MonthDropdownButton(
-                        selectedMonth: selectedMonth,
-                        onMonthChanged: (value) {
-                          setState(() {
-                            selectedMonth = value; // 선택된 월 업데이트
-                          });
-                        },
-                        fontSize: 32,
-                      ),
-                    ],
+                        const AssetList(),
+                      ],
+                    ),
                   ),
-                  const AssetList(),
+                  TableCalendar<TransactionDetail>(
+                    headerStyle: const HeaderStyle(
+                      formatButtonVisible: false,
+                      titleCentered: true,
+                    ),
+                    locale: 'ko-KR',
+                    firstDay: DateTime.utc(2020, 1, 1),
+                    lastDay: DateTime.utc(2030, 12, 31),
+                    focusedDay: _focusedDay,
+                    selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+                    calendarFormat: _calendarFormat,
+                    availableGestures: AvailableGestures.horizontalSwipe,
+                    onDaySelected: _onDaySelected,
+                    eventLoader: _getEventsForDay,
+                    onPageChanged: (focusedDay) {
+                      _focusedDay = focusedDay;
+                    },
+                  ),
+                  const SizedBox(height: 8.0),
+                  const Divider(),
+                  ValueListenableBuilder<List<TransactionDetail>>(
+                    valueListenable: _selectedEvents,
+                    builder: (context, value, _) {
+                      return ListView.builder(
+                        shrinkWrap: true, // ListView의 크기를 제한
+                        physics: const NeverScrollableScrollPhysics(), // 스크롤 비활성화
+                        itemCount: value.length,
+                        itemBuilder: (context, index) {
+                          final transaction = value[index];
+                          // return ListTile(
+                          //   title: Text(transaction.title),
+                          //   subtitle: Text('Amount: ${transaction.amount}'),
+                          //   leading: Image.network(transaction.imgUrl),
+                          // );
+                          return Padding(
+                            padding: const EdgeInsets.only(bottom: 16.0),
+                            child: Column(
+                              children: [
+                                TransactionItem(
+                                  title: transaction.title,
+                                  amount: transaction.amount,
+                                  imgUrl: transaction.imgUrl,
+                                  createdAt: transaction.createdAt.toString(),
+                                  category: transaction.category,
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  ),
+                  const Divider(),
                 ],
               ),
             ),
-            const SizedBox(height: 16),
-            TableCalendar(
-              // headerVisible: false,
-              headerStyle: const HeaderStyle(
-                formatButtonVisible: false,
-                titleCentered: true,
-              ),
-              locale: 'ko-KR',
-              firstDay: DateTime.utc(2010, 10, 16),
-              lastDay: DateTime.utc(2030, 3, 14),
-              focusedDay: focusedDay,
-              availableGestures: AvailableGestures.horizontalSwipe,
-              // onHeaderTapped: (focusedDay) async {
-              //   final DateTime? dateTime = await showDatePicker(
-              //     context: context,
-              //     initialDate: now,
-              //     firstDate: DateTime(2000),
-              //     lastDate: DateTime(3000),
-              //   );
-              //   if (dateTime != null) {
-              //     setState(() {
-              //       now = dateTime;
-              //     });
-              //   }
-              // },
-            ),
-            const Divider(),
-            const SortButton(),
-            const TransactionList(),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
