e8d6eec96c9e4cd950a9aa3c77a019e861dee565
itbebop <it.bebop@gmail.com>
2024-10-08 23:46:08 +0900

    chore: 앱 이름 변경, sdkversion 23으로 변경, Kakao activity추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_moneybag_2024/app.dart';
+import 'package:flutter_moneybag_2024/firebase_options.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 void main() async {
+  await dotenv.load(fileName: '.env');
+  // kakao 초기화
+  WidgetsFlutterBinding.ensureInitialized();
+  KakaoSdk.init(
+    nativeAppKey: dotenv.get('KAKAO_NATIVE_APP_KEY'),
+    javaScriptAppKey: dotenv.get('KAKAO_JAVASCRIPT_APP_KEY'),
+  );
+
+  // firebase 초기화
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   await initializeDateFormatting('ko', null);
   runApp(const MyApp());
 }
