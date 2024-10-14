b69fc1679d034bc7a6d00b117ebabfd9a5656712
itbebop <it.bebop@gmail.com>
2024-10-14 22:06:26 +0900

    feat: asset 화면에 들어갈 animation sample

--- lib/screen/tab/asset/asset_screen.dart
+++ lib/screen/tab/asset/asset_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_moneybag_2024/screen/tab/asset/counter_text.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class AssetScreen extends ConsumerStatefulWidget {
@@ -11,6 +12,6 @@ class AssetScreen extends ConsumerStatefulWidget {
 class _ReportScreenState extends ConsumerState<AssetScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const TweenDemo();
   }
 }
