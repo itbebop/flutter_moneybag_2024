b78e65d8143bc660bd920afd4be327b58a46246c
itbebop <it.bebop@gmail.com>
2024-10-16 17:13:05 +0900

    feat: firebase convert AssetType(enum)

--- lib/data/data_source/transaction_category_data_source_impl.dart
+++ lib/data/data_source/transaction_category_data_source_impl.dart
@@ -1,11 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_moneybag_2024/data/data_source/transaction_category_data_source.dart';
+import 'package:flutter_moneybag_2024/domain/enums/asset_types.dart';
 import 'package:flutter_moneybag_2024/domain/model/transaction_category.dart';
 
 class TransactionCategoryDataSourceImpl implements TransactionCategoryDataSource {
-  final _transactionCategoryRef = FirebaseFirestore.instance
-      .collection('transactionCategory')
-      .withConverter<TransactionCategory>(fromFirestore: (snapshot, _) => TransactionCategory.fromJson(snapshot.data()!), toFirestore: (snapshot, _) => snapshot.toJson());
+  final _transactionCategoryRef = FirebaseFirestore.instance.collection('transactionCategory').withConverter<TransactionCategory>(
+        fromFirestore: (snapshot, _) => TransactionCategory.fromJson(snapshot.data()!),
+        toFirestore: (transactionCategory, _) {
+          final transactionCategoryMap = transactionCategory.toJson();
+          transactionCategoryMap['AssetType'] = transactionCategory.type.displayName;
+          return transactionCategoryMap;
+        },
+      );
 
   @override
   Future<void> createTransactionCategory({required TransactionCategory transactionCategory}) async {
