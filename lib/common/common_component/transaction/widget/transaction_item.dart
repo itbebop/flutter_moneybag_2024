cd85c2674ca1f99e4dee2ed0684d7580e17f5693
itbebop <it.bebop@gmail.com>
2024-10-17 00:00:28 +0900

    style: 너비 늘림

--- lib/common/common_component/transaction/widget/transaction_item.dart
+++ lib/common/common_component/transaction/widget/transaction_item.dart
@@ -14,7 +14,7 @@ class TransactionItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+      padding: const EdgeInsets.symmetric(horizontal: 8),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
