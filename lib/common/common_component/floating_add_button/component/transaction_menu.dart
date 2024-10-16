3211382c75c49e3536ba88d1346f816cd5fb5920
itbebop <it.bebop@gmail.com>
2024-10-16 16:40:00 +0900

    feat: write 부분 추가

--- lib/common/common_component/floating_add_button/component/transaction_menu.dart
+++ lib/common/common_component/floating_add_button/component/transaction_menu.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_moneybag_2024/common/common.dart';
 import 'package:flutter_moneybag_2024/common/common_component/floating_add_button/component/float_item.dart';
+import 'package:flutter_moneybag_2024/common/common_component/transaction/riverpod/transaction_state_notifier.dart';
+import 'package:flutter_moneybag_2024/core/provider/user_state_notifier.dart';
+import 'package:flutter_moneybag_2024/domain/enums/asset_types.dart';
 import 'package:flutter_moneybag_2024/domain/model/dummies.dart';
+import 'package:flutter_moneybag_2024/domain/model/transaction_category.dart';
+import 'package:flutter_moneybag_2024/domain/model/transaction_detail.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class TransactionMenu extends StatelessWidget {
+class TransactionMenu extends ConsumerWidget {
   const TransactionMenu({
     super.key,
     required this.duration,
@@ -14,7 +21,8 @@ class TransactionMenu extends StatelessWidget {
   final bool isClassified;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userState = ref.watch(userStateProvier);
     final memoEditController = TextEditingController();
     final amountEditController = TextEditingController();
 
@@ -44,6 +52,10 @@ class TransactionMenu extends StatelessWidget {
                   ),
                   child: TextFormField(
                     controller: amountEditController,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly, // 숫자만 허용
+                    ],
                     textAlignVertical: TextAlignVertical.bottom,
                     style: const TextStyle(color: Colors.black),
                     cursorColor: const Color(0xFF075E54),
@@ -99,15 +111,32 @@ class TransactionMenu extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(height: 8),
-                Container(
-                  padding: const EdgeInsets.only(top: 12, bottom: 12, left: 36, right: 36),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: UiConfig.primaryColorSurface,
-                  ),
-                  child: Text(
-                    '입력',
-                    style: UiConfig.h4Style,
+                Tap(
+                  onTap: () async {
+                    await ref.read(userStateProvier.notifier).fetchUser();
+                    if (userState.value != null) {
+                      ref.read(transactionStateProvider.notifier).createTransaction(TransactionDetail(
+                          transactionId: '1',
+                          title: memoEditController.text,
+                          createdAt: DateTime.now().subtract(2.days),
+                          updatedAt: DateTime.now().subtract(2.days),
+                          amount: double.parse(amountEditController.text),
+                          userId: [userState.value!.userId],
+                          category: TransactionCategory(id: '1', name: '이자', type: AssetType.income, imgUrl: picSum(201), userId: 'kpbwsziudRcomCD9mLx0o4QUHQq1')));
+                    } else {
+                      // TODO: 스낵바 띄우기
+                    }
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.only(top: 12, bottom: 12, left: 36, right: 36),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: UiConfig.primaryColorSurface,
+                    ),
+                    child: Text(
+                      '입력',
+                      style: UiConfig.h4Style,
+                    ),
                   ),
                 )
               ],
