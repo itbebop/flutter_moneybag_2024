956b2f4f43195b344719ea457a14e49342decd0e
itbebop <it.bebop@gmail.com>
2024-10-29 16:14:10 +0900

    style: asset update ui 변경

--- lib/screen/tab/asset/component/asset_card_update.dart
+++ lib/screen/tab/asset/component/asset_card_update.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_moneybag_2024/common/common.dart';
-import 'package:flutter_moneybag_2024/common/widget/custom_button.dart';
 import 'package:flutter_moneybag_2024/common/widget/custom_dropdown_button.dart';
-import 'package:flutter_moneybag_2024/core/provider/user_state_notifier.dart';
 import 'package:flutter_moneybag_2024/domain/enums/currency.dart';
 import 'package:flutter_moneybag_2024/domain/model/asset.dart';
-import 'package:flutter_moneybag_2024/screen/tab/asset/component/color_picker_widget.dart';
 import 'package:flutter_moneybag_2024/screen/tab/asset/riverpod/asset_state_notifier.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hugeicons/hugeicons.dart';
 
 class AssetCardUpdate extends ConsumerWidget {
   final TextEditingController titleEditController;
@@ -112,71 +108,6 @@ class AssetCardUpdate extends ConsumerWidget {
               ),
             ],
           ),
-          SizedBox(height: 16.h),
-          SizedBox(
-            height: 150.h,
-            width: 350.w,
-            child: const ColorPickerWidget(isFirst: true),
-          ),
-          SizedBox(
-            height: 150.h,
-            width: 350.w,
-            child: const ColorPickerWidget(isFirst: false),
-          ),
-          SizedBox(height: 16.h),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Tap(
-                onTap: () => ref.read(assetStateProvier.notifier).onTapAssetCardUpdate(false),
-                child: CustomButton(
-                  name: '취 소',
-                  buttonColor: UiConfig.buttonColor,
-                  textStyle: UiConfig.extraSmallStyle.copyWith(
-                    fontWeight: UiConfig.semiBoldFont,
-                  ),
-                ),
-              ),
-              SizedBox(width: 16.w),
-              Tap(
-                onTap: () {
-                  ref.read(assetStateProvier.notifier).updateAsset(
-                        Asset(
-                          totalAmount: asset.totalAmount,
-                          totalIncome: asset.totalIncome,
-                          totalExpense: asset.totalExpense,
-                          assetId: asset.assetId,
-                          assetName: assetProvider.assetName,
-                          currency: assetProvider.currencyHints,
-                          userIdList: asset.userIdList,
-                          createdAt: asset.createdAt,
-                          updatedAt: DateTime.now(),
-                          assetColor: [
-                            assetProvider.firstColor.alpha,
-                            assetProvider.firstColor.red,
-                            assetProvider.firstColor.green,
-                            assetProvider.firstColor.blue,
-                            assetProvider.secondColor.alpha,
-                            assetProvider.secondColor.red,
-                            assetProvider.secondColor.green,
-                            assetProvider.secondColor.blue
-                          ],
-                        ),
-                      );
-                  ref.read(assetStateProvier.notifier).fetchAsset();
-                  ref.read(assetStateProvier.notifier).onTapAssetCardUpdate(false);
-                  ref.read(userStateProvider.notifier).modifyColorList(assetProvider.firstColorList, assetProvider.secondColorList);
-                },
-                child: CustomButton(
-                  name: '확 인',
-                  buttonColor: UiConfig.buttonColor,
-                  textStyle: UiConfig.extraSmallStyle.copyWith(
-                    fontWeight: UiConfig.semiBoldFont,
-                  ),
-                ),
-              ),
-            ],
-          )
         ],
       ),
     );
