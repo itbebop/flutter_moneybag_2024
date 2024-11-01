6cc665414014c4e4f329c935269031d1b8c557c2
itbebop <it.bebop@gmail.com>
2024-11-01 16:45:55 +0900

    feat: category update 필요해서 다시 추가

--- lib/domain/repository/transaction_category_repository.dart
+++ lib/domain/repository/transaction_category_repository.dart
@@ -3,5 +3,6 @@ import 'package:flutter_moneybag_2024/domain/model/transaction_category.dart';
 abstract interface class TransactionCategoryRepository {
   Future<void> createTransactionCategory({required TransactionCategory transactionCategory, required String userId});
   Future<List<TransactionCategory>> getTransactionCategoryList({required String userId});
+  Future<void> updateTransactionCategory({required TransactionCategory transactionCategory, required String userId});
   Future<void> deleteTransactionCategory({required String categoryId, required String userId});
 }
