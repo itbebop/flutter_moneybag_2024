df082814037bec282fbe60c2eb886416db0af676
itbebop <it.bebop@gmail.com>
2024-11-05 13:37:09 +0900

    feat: transaction data 받는 것으로 변경 - yearly, monthly, weekly로 나눔 - yearlyData부터 예시 데이터로 맞춰보는중 - y축 기준 동적으로 변경(LineChartData yearlyData) - x축은 year, month, week에 맞게 일단 고정값으로 설정

--- lib/screen/tab/report/chart/line_chart_base.dart
+++ lib/screen/tab/report/chart/line_chart_base.dart
@@ -1,194 +1,264 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_moneybag_2024/domain/enums/period_types.dart';
+import 'package:flutter_moneybag_2024/domain/model/transaction_detail.dart';
 import 'package:flutter_moneybag_2024/screen/tab/report/chart/temporary_app_color.dart';
 
 class LineChartBase extends StatelessWidget {
   const LineChartBase({
     super.key,
-    required this.isShowingMainData,
     required this.period,
+    required this.transactionList,
   });
 
-  final bool isShowingMainData;
   final Period period;
+  final List<TransactionDetail> transactionList;
+
   @override
   Widget build(BuildContext context) {
+    // Filter transactionList based on `createdAt` and the selected `period`
+    final filteredTransactionList = filterTransactionsByPeriod(transactionList);
+
     return LineChart(
-      // isShowingMainData ? sampleData1 : sampleData2,
-      sampleData1,
+      chartData(filteredTransactionList),
       duration: const Duration(milliseconds: 250),
     );
   }
 
-  LineChartData get sampleData1 => LineChartData(
-        lineTouchData: lineTouchData1,
+  // Method to filter transactions based on the selected period
+  List<TransactionDetail> filterTransactionsByPeriod(List<TransactionDetail> transactions) {
+    final now = DateTime.now();
+    return transactions.where((transaction) {
+      final date = transaction.createdAt;
+      switch (period) {
+        case Period.year:
+          return date.year == now.year;
+        case Period.month:
+          return date.year == now.year && date.month == now.month;
+        case Period.week:
+        default:
+          final startOfWeek = now.subtract(Duration(days: now.weekday - 1)); // Monday as start of week
+          return date.isAfter(startOfWeek) && date.isBefore(startOfWeek.add(const Duration(days: 7)));
+      }
+    }).toList();
+  }
+
+  LineChartData chartData(List<TransactionDetail> filteredTransactionList) {
+    switch (period) {
+      case Period.year:
+        return yearlyData(filteredTransactionList);
+      case Period.month:
+        return monthlyData(filteredTransactionList);
+      case Period.week:
+      default:
+        return weeklyData(filteredTransactionList);
+    }
+  }
+
+  // Define the LineChartData methods with updated minX, maxX, and custom title configurations
+  LineChartData yearlyData(List<TransactionDetail> transactionList) {
+    // amount 값이 양수인지 음수인지 확인
+    bool hasPositive = transactionList.any((transaction) => transaction.amount > 0);
+    bool hasNegative = transactionList.any((transaction) => transaction.amount < 0);
+
+    // minY와 maxY 설정
+    double minY;
+    double maxY;
+
+    if (hasPositive && hasNegative) {
+      minY = -3;
+      maxY = 3;
+    } else if (hasPositive) {
+      minY = 0;
+      maxY = 5;
+    } else if (hasNegative) {
+      minY = -5;
+      maxY = 0;
+    } else {
+      // 양수, 음수 모두 없을 때 기본 값 설정 (필요시 조정)
+      minY = 0;
+      maxY = 0;
+    }
+
+    return LineChartData(
+      lineTouchData: lineTouchData,
+      gridData: gridData,
+      titlesData: titlesData(transactionList),
+      borderData: borderData,
+      lineBarsData: lineBarsData(transactionList),
+      minX: 0,
+      maxX: 5,
+      maxY: maxY,
+      minY: minY,
+    );
+  }
+
+  LineChartData monthlyData(List<TransactionDetail> transactionList) => LineChartData(
+        lineTouchData: lineTouchData,
         gridData: gridData,
-        titlesData: titlesData1,
+        titlesData: titlesData(transactionList),
         borderData: borderData,
-        lineBarsData: lineBarsData1,
+        lineBarsData: lineBarsData(transactionList),
         minX: 0,
-        maxX: 14,
-        maxY: 4,
+        maxX: 4, // 4 weeks in a month
+        maxY: 5,
         minY: 0,
       );
 
-  LineChartData get sampleData2 => LineChartData(
-        lineTouchData: lineTouchData2,
+  LineChartData weeklyData(List<TransactionDetail> transactionList) => LineChartData(
+        lineTouchData: lineTouchData,
         gridData: gridData,
-        titlesData: titlesData2,
+        titlesData: titlesData(transactionList),
         borderData: borderData,
-        lineBarsData: lineBarsData2,
+        lineBarsData: lineBarsData(transactionList),
         minX: 0,
-        maxX: 14,
-        maxY: 6,
+        maxX: 6, // 7 days in a week
+        maxY: 5,
+        // maxY: maxY(transactionList, 7),
         minY: 0,
       );
 
-  LineTouchData get lineTouchData1 => LineTouchData(
+  // Reusable line touch data across charts
+  LineTouchData get lineTouchData => LineTouchData(
         handleBuiltInTouches: true,
         touchTooltipData: LineTouchTooltipData(
           getTooltipColor: (touchedSpot) => Colors.blueGrey.withOpacity(0.8),
         ),
       );
 
-  FlTitlesData get titlesData1 => FlTitlesData(
-        bottomTitles: AxisTitles(
-          sideTitles: bottomTitles,
-        ),
-        rightTitles: const AxisTitles(
-          sideTitles: SideTitles(showTitles: false),
-        ),
-        topTitles: const AxisTitles(
-          sideTitles: SideTitles(showTitles: false),
-        ),
-        leftTitles: AxisTitles(
-          sideTitles: leftTitles(),
-        ),
-      );
-
-  List<LineChartBarData> get lineBarsData1 => [
-        lineChartBarData1_1,
-        lineChartBarData1_2,
-        lineChartBarData1_3,
-      ];
-
-  LineTouchData get lineTouchData2 => const LineTouchData(
-        enabled: false,
-      );
-
-  FlTitlesData get titlesData2 => FlTitlesData(
-        bottomTitles: AxisTitles(
-          sideTitles: bottomTitles,
-        ),
-        rightTitles: const AxisTitles(
-          sideTitles: SideTitles(showTitles: false),
-        ),
-        topTitles: const AxisTitles(
-          sideTitles: SideTitles(showTitles: false),
-        ),
-        leftTitles: AxisTitles(
-          sideTitles: leftTitles(),
-        ),
-      );
-
-  List<LineChartBarData> get lineBarsData2 => [
-        lineChartBarData2_1,
-        lineChartBarData2_2,
-        lineChartBarData2_3,
-      ];
-
-  Widget leftTitleWidgets(double value, TitleMeta meta) {
-    const style = TextStyle(
-      fontWeight: FontWeight.bold,
-      fontSize: 12,
+  FlTitlesData titlesData(List<TransactionDetail> transactionList) {
+    return FlTitlesData(
+      bottomTitles: AxisTitles(
+        sideTitles: bottomTitles,
+      ),
+      leftTitles: AxisTitles(
+        sideTitles: leftTitles(transactionList),
+      ),
+      topTitles: const AxisTitles(
+        // Disable top titles
+        sideTitles: SideTitles(showTitles: false),
+      ),
+      rightTitles: const AxisTitles(
+        // Disable right titles
+        sideTitles: SideTitles(showTitles: false),
+      ),
     );
-    String text;
-    switch (value.toInt()) {
+  }
+
+  Widget leftTitleWidgets(double value, TitleMeta meta, List<TransactionDetail> transactionList) {
+    final maxTransactionAmount = transactionList.map((t) => t.amount).reduce((a, b) => a > b ? a : b);
+    print('#####maxTransactionAmount: $maxTransactionAmount');
+    // final multiplier = period == Period.year
+    //     ? 365
+    //     : period == Period.month
+    //         ? 30
+    //         : 7;
+    double maxPoint = 0;
+    if (maxTransactionAmount > 1e4) {
+      maxPoint = maxTransactionAmount.toInt() / 1e4;
+    }
+
+    final double firstPoint = maxPoint / 5;
+
+    double point;
+
+    switch (value) {
       case 1:
-        text = '50만';
+        point = firstPoint;
         break;
       case 2:
-        text = '100만';
+        point = firstPoint * 2;
         break;
       case 3:
-        text = '150만';
+        point = firstPoint * 3;
         break;
       case 4:
-        text = '200만';
+        point = firstPoint * 4;
         break;
       case 5:
-        text = '250만';
+        point = maxPoint;
+        break;
+      case -1:
+        point = -firstPoint;
+        break;
+      case -2:
+        point = -firstPoint * 2;
+        break;
+      case -3:
+        point = -firstPoint * 3;
+        break;
+      case -4:
+        point = -firstPoint * 4;
+        break;
+      case -5:
+        point = -maxPoint;
         break;
       default:
         return Container();
     }
-
-    return Text(text, style: style, textAlign: TextAlign.center);
+    return Text('${(point.toInt()).toString()}만', textAlign: TextAlign.center, style: const TextStyle(fontSize: 12));
   }
 
-  SideTitles leftTitles() => SideTitles(
-        getTitlesWidget: leftTitleWidgets,
+  SideTitles leftTitles(List<TransactionDetail> transactionList) => SideTitles(
+        getTitlesWidget: (value, meta) => leftTitleWidgets(value, meta, transactionList),
         showTitles: true,
         interval: 1,
         reservedSize: 40,
       );
 
   Widget bottomTitleWidgets(double value, TitleMeta meta) {
-    const style = TextStyle(
-      fontWeight: FontWeight.bold,
-      fontSize: 14,
-    );
-    Widget text;
-
+    const style = TextStyle(fontSize: 12);
+    String text;
     switch (period) {
+      case Period.year:
+        text = yearlyBottomTitle(value);
+        break;
       case Period.month:
-        switch (value.toInt()) {
-          case 2:
-            text = const Text('SEPT', style: style);
-            break;
-          case 7:
-            text = const Text('OCT', style: style);
-            break;
-          case 12:
-            text = const Text('DEC', style: style);
-            break;
-          default:
-            text = const Text('');
-            break;
-        }
+        text = monthlyBottomTitle(value);
+        break;
       case Period.week:
       default:
-        switch (value.toInt()) {
-          case 1:
-            text = const Text('Jan', style: style);
-            break;
-
-          case 4:
-            text = const Text('Apr', style: style);
-            break;
-
-          case 7:
-            text = const Text('Jul', style: style);
-            break;
-
-          case 10:
-            text = const Text('Oct', style: style);
-            break;
-          case 13:
-            text = const Text('Dec', style: style);
-            break;
-
-          default:
-            text = const Text('');
-            break;
-        }
+        text = weeklyBottomTitle(value);
+        break;
     }
-    return SideTitleWidget(
-      axisSide: meta.axisSide,
-      space: 10,
-      child: text,
-    );
+    return SideTitleWidget(axisSide: meta.axisSide, space: 10, child: Text(text, style: style));
+  }
+
+  String yearlyBottomTitle(double value) {
+    switch (value.toInt()) {
+      case 0:
+        return 'Jan';
+      case 3:
+        return 'Apr';
+      case 6:
+        return 'Jul';
+      case 9:
+        return 'Oct';
+      case 12:
+        return 'Dec';
+      default:
+        return '';
+    }
+  }
+
+  String monthlyBottomTitle(double value) {
+    final weekNum = value.toInt() + 1;
+    return 'Week $weekNum';
+  }
+
+  String weeklyBottomTitle(double value) {
+    switch (value.toInt()) {
+      case 0:
+        return 'Mon';
+      case 1:
+        return 'Wed';
+      case 2:
+        return 'Fri';
+      case 3:
+        return 'Sun';
+      default:
+        return '';
+    }
   }
 
   SideTitles get bottomTitles => SideTitles(
@@ -209,114 +279,41 @@ class LineChartBase extends StatelessWidget {
           top: const BorderSide(color: Colors.transparent),
         ),
       );
+  List<LineChartBarData> lineBarsData(List<TransactionDetail> yearlyData) {
+    // 각 월의 amount 합계를 계산하고, FlSpot 리스트를 생성
+    List<FlSpot> createYearlySpots(List<TransactionDetail> dataList) {
+      // 월별 데이터 그룹화 후 amount 합산
+      Map<int, double> monthlySums = {};
+      for (var transaction in dataList) {
+        int month = transaction.createdAt.month;
+        monthlySums[month] = (monthlySums[month] ?? 0) + transaction.amount;
+      }
 
-  LineChartBarData get lineChartBarData1_1 => LineChartBarData(
+      // FlSpot 생성, 없는 월은 제외
+      return monthlySums.entries.map((entry) {
+        double x = entry.key.toDouble();
+        double y = entry.value / 1e6; // 1e6으로 나눠서 단위를 맞춤
+        return FlSpot(x, y);
+      }).toList();
+    }
+
+    return [
+      LineChartBarData(
         isCurved: true,
-        color: AppColors.contentColorGreen,
+        color: Colors.blue,
         barWidth: 4,
         isStrokeCapRound: true,
         dotData: const FlDotData(show: false),
         belowBarData: BarAreaData(show: false),
         spots: const [
           FlSpot(1, 1),
-          FlSpot(3, 1.5),
-          FlSpot(5, 1.4),
-          FlSpot(7, 3.4),
-          FlSpot(10, 2),
-          FlSpot(12, 2.2),
-          FlSpot(13, 1.8),
+          FlSpot(3, -2.8),
+          FlSpot(7, -1.2),
+          FlSpot(10, -2.8),
+          FlSpot(12, -2.6),
+          FlSpot(13, -3.9),
         ],
-      );
-
-  LineChartBarData get lineChartBarData1_2 => LineChartBarData(
-        isCurved: true,
-        color: AppColors.contentColorPink,
-        barWidth: 4,
-        isStrokeCapRound: true,
-        dotData: const FlDotData(show: false),
-        belowBarData: BarAreaData(
-          show: false,
-          color: AppColors.contentColorPink.withOpacity(0),
-        ),
-        spots: const [
-          FlSpot(1, 1),
-          FlSpot(3, 2.8),
-          FlSpot(7, 1.2),
-          FlSpot(10, 2.8),
-          FlSpot(12, 2.6),
-          FlSpot(13, 3.9),
-        ],
-      );
-
-  LineChartBarData get lineChartBarData1_3 => LineChartBarData(
-        isCurved: true,
-        color: AppColors.contentColorCyan,
-        barWidth: 4,
-        isStrokeCapRound: true,
-        dotData: const FlDotData(show: false),
-        belowBarData: BarAreaData(show: false),
-        spots: const [
-          FlSpot(1, 2.8),
-          FlSpot(3, 1.9),
-          FlSpot(6, 3),
-          FlSpot(10, 1.3),
-          FlSpot(13, 2.5),
-        ],
-      );
-
-  LineChartBarData get lineChartBarData2_1 => LineChartBarData(
-        isCurved: true,
-        curveSmoothness: 0,
-        color: AppColors.contentColorGreen.withOpacity(0.5),
-        barWidth: 4,
-        isStrokeCapRound: true,
-        dotData: const FlDotData(show: false),
-        belowBarData: BarAreaData(show: false),
-        spots: const [
-          FlSpot(1, 1),
-          FlSpot(3, 4),
-          FlSpot(5, 1.8),
-          FlSpot(7, 5),
-          FlSpot(10, 2),
-          FlSpot(12, 2.2),
-          FlSpot(13, 1.8),
-        ],
-      );
-
-  LineChartBarData get lineChartBarData2_2 => LineChartBarData(
-        isCurved: true,
-        color: AppColors.contentColorPink.withOpacity(0.5),
-        barWidth: 4,
-        isStrokeCapRound: true,
-        dotData: const FlDotData(show: false),
-        belowBarData: BarAreaData(
-          show: true,
-          color: AppColors.contentColorPink.withOpacity(0.2),
-        ),
-        spots: const [
-          FlSpot(1, 1),
-          FlSpot(3, 2.8),
-          FlSpot(7, 1.2),
-          FlSpot(10, 2.8),
-          FlSpot(12, 2.6),
-          FlSpot(13, 3.9),
-        ],
-      );
-
-  LineChartBarData get lineChartBarData2_3 => LineChartBarData(
-        isCurved: true,
-        curveSmoothness: 0,
-        color: AppColors.contentColorCyan.withOpacity(0.5),
-        barWidth: 2,
-        isStrokeCapRound: true,
-        dotData: const FlDotData(show: true),
-        belowBarData: BarAreaData(show: false),
-        spots: const [
-          FlSpot(1, 3.8),
-          FlSpot(3, 1.9),
-          FlSpot(6, 5),
-          FlSpot(10, 3.3),
-          FlSpot(13, 4.5),
-        ],
-      );
+      ),
+    ];
+  }
 }
